18ee7d220d37059c9c69f7b3c959712309bc1f67
sangkyuleeKOR <lyk10045@naver.com>
2019-05-06 16:46:06 +0900

    day_!

--- Ex_1.py
+++ Ex_1.py
@@ -1,1 +1,19 @@
-import numpy as np
+'''
+b=0
+while True:
+    try:
+        a = input()
+        b+=1
+        if len(a)>100:
+            break
+        if b>100:
+            break
+        if a==' ':
+            break
+        print(a)
+    except EOFError:
+        break
+''' # EOFerror활용해서 예외처리
+
+
+
